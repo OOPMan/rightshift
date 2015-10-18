70ec0af5c07f076e248c4eab4624a1a53e0fe525
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2015-10-18 09:13:09 +0200

    Added skeleton packages and updated setup.py.

--- setup.py
+++ setup.py
@@ -3,10 +3,19 @@ from distutils.core import setup
 setup(
     name='rightshift',
     version='0.1.0',
-    packages=[''],
+    packages=['rightshift'],
     url='https://github.com/OOPMan/rightshift',
     license='MIT',
     author='Adam Jorgensen',
     author_email='adam.jorgensen.za@gmail.com',
-    description='RightShift is a Python package for writing chained operations in a readable fashion.'
+    description='RightShift is a Python package for writing chained operations in a readable fashion.',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
 )
