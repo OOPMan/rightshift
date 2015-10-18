c969d3f140b0271ed5193eac4484adaa0e2080be
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2015-10-18 17:38:37 +0200

    Implemented PatternGroupExtractor.

--- rightshift/extractors.py
+++ rightshift/extractors.py
@@ -4,7 +4,11 @@ from future.utils import raise_from
 __author__ = 'adam.jorgensen.za@gmail.com'
 
 
-class _ItemExtractor(Transformer):
+class Extractor(Transformer):
+    pass
+
+
+class _ItemExtractor(Extractor):
     """
     An ItemExtract instances expects to be called with a value that will be
     accessed as if it were a container type in order to retrieve the item or
@@ -28,5 +32,26 @@ class _ItemExtractor(Transformer):
 item = _ItemExtractor()
 
 
+class _PatternGroupExtractor(Extractor):
+
+    def __init__(self, pattern, group=1, search=True):
+        from past.builtins import basestring
+        if isinstance(pattern, basestring):
+            from re import compile
+            pattern = compile(pattern)
+        self.pattern = pattern
+        self.group = group
+        self.search = search
+
+    def __call__(self, value):
+        try:
+            method = self.pattern.search if self.search else self.pattern.match
+            match = method(value)
+            if match is None:
+                raise TransformationException
+            return match.group(self.group)
+        except Exception as e:
+            raise_from(TransformationException, e)
+pattern_group = _PatternGroupExtractor
 
 
