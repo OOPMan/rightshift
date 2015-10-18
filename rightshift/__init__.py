102e36f3bd0b7cce6a509bf42c62b55e24bf9879
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2015-10-18 16:43:54 +0200

    Implemented basic Transformer and TransformerException classes.

--- rightshift/__init__.py
+++ rightshift/__init__.py
@@ -1,1 +1,23 @@
-__author__ = 'adamj'
+__author__ = 'adam.jorgensen.za@gmail.com'
+
+
+class TransformationException(BaseException):
+    """
+    An exception that is raised when a Transform is unable to transform a value
+    to another value.
+    """
+    pass
+
+
+class Transformer(object):
+    """
+    A Transform is an object which can be called with a single input value.
+
+    If the transformation succeeds, a value will be returned. The returned value
+    may be None.
+
+    If the transformation fails, a TransformationException will be raised.
+    """
+
+    def __call__(self, value):
+        raise NotImplementedError
