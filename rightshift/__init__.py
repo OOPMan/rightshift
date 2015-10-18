01c2d64f47a27d90e6c9068a1ef5ad98feac6cfa
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2015-10-18 17:40:36 +0200

    Added abstract __rshift__ method to Transformer class.

--- rightshift/__init__.py
+++ rightshift/__init__.py
@@ -17,7 +17,15 @@ class Transformer(object):
     may be None.
 
     If the transformation fails, a TransformationException will be raised.
+
+    A Transform may be chained together with other Transforms using the >>
+    operation. Chaining functions such that Transforms can be chained
+    together from left to right in the order they should logically executed on
+    the input value.
     """
 
     def __call__(self, value):
+        raise NotImplementedError
+
+    def __rshift__(self, other):
         raise NotImplementedError
