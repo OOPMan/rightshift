d72ee58fad53ede5f848072e45eeb23d6148231c
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2015-10-18 08:29:51 +0200

    Added setup.py file.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='rightshift',
+    version='0.1.0',
+    packages=[''],
+    url='https://github.com/OOPMan/rightshift',
+    license='MIT',
+    author='Adam Jorgensen',
+    author_email='adam.jorgensen.za@gmail.com',
+    description='RightShift is a Python package for writing chained operations in a readable fashion.'
+)
