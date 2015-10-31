205030a3932e2d32a84210f48cc76842cbd07fab
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2015-10-31 16:31:29 +0200

    Re-wrote Comparison class and sub-classes somewhat. Added more documentation.

--- rightshift/matchers.py
+++ rightshift/matchers.py
@@ -1,4 +1,6 @@
 from copy import copy
+from future.utils import raise_from
+
 from rightshift import Transformer, RightShiftException
 
 __author__ = 'adam.jorgensen.za@gmail.com'
@@ -205,87 +207,170 @@ is_instance is a an alias to the IsInstance class within this module.
 
 class Comparison(Matcher):
     """
-    Base Matcher class for comparison Matchers.
+    A Comparison is a Matcher that should be instantiated with a callable
+    object.
 
-    Sub-classes of this should implement the __call__ method. The included
-    sub-classes of Comparison do not perform any kind of type check when
-    performing a comparison in order to enable comparison operator overloading
-    on the operand values to function.
+    When the Comparison instance is called it will in turn call the comparator
+    it was instantiated with in order to obtain a boolean result indicating
+    the result of the comparison.
+
+    Additionally, Comparison can be instructed to convert Exceptions during the
+    call to the comparator into False results. This can be done either at
+    instantiation using the falsey_exceptions parameter or at call time using
+    the comparison__falsey_exceptions flag.
+
+    Comparison is sub-classed in order to implement the included LessThan,
+    LessThanEqualTo, EqualTo, NotEqualTo, GreaterThanEqualTo and GreaterThan
+    matchers.
     """
-    def __init__(self, value):
+    def __init__(self, comparator, falsey_exceptions=False):
         """
-        TODO: Document
+        :param comparator: A callable object with the signature f(value, **flags)
+        :param falsey_exceptions: A boolean value indicating whether exceptions
+                                  during comparison should be converted to False
         """
-        self.value = value
+        if not callable(comparator):
+            raise MatcherException('{} is not callable'.format(comparator))
+        self.comparator = comparator
+        self.falsey_exceptions = falsey_exceptions
 
     def __call__(self, value, **flags):
+        try:
+            return bool(self.comparator(value, **flags))
+        except Exception as e:
+            if flags.get('comparison__falsey_exceptions', self.falsey_exceptions):
+                return False
+            raise_from(MatcherException, e)
         raise NotImplementedError
+
+compare_using = comparison = Comparison
+"""
+An alias to the Comparison class.
+
+Examples:
+
+compare_using(lambda v, **flags: v.startswith('x'))
+"""
 
 
 class LessThan(Comparison):
     """
     A Less Than comparison.
     """
-    def __call__(self, value, **flags):
+    def __init__(self, value, falsey_exceptions=False):
+        super(LessThan, self).__init__(self.__compare, falsey_exceptions)
+        self.value = value
+
+    def __compare(self, value, **flags):
         """
         TODO: Document
         """
         return value < self.value
+
+lt = less_than = LessThan
+"""
+An alias to the LessThan class.
+"""
 
 
 class LessThanEqualTo(Comparison):
     """
     A Less than or equal to comparison.
     """
-    def __call__(self, value, **flags):
+    def __init__(self, value, falsey_exceptions=False):
+        super(LessThanEqualTo, self).__init__(self.__compare, falsey_exceptions)
+        self.value = value
+
+    def __compare(self, value, **flags):
         """
         TODO: Document
         """
         return value <= self.value
+
+lte = less_than_equal_to = LessThanEqualTo
+"""
+An alias to the LessThanEqualTo class.
+"""
 
 
 class EqualTo(Comparison):
     """
     An equal to comparison.
     """
-    def __call__(self, value, **flags):
+    def __init__(self, value, falsey_exceptions=False):
+        super(EqualTo, self).__init__(self.__compare, falsey_exceptions)
+        self.value = value
+
+    def __compare(self, value, **flags):
         """
         TODO: Document
         """
         return value == self.value
+
+eq = equal_to = EqualTo
+"""
+An alias to the EqualTo class.
+"""
 
 
 class NotEqualTo(Comparison):
     """
     A not equal to comparison.
     """
-    def __call__(self, value, **flags):
+    def __init__(self, value, falsey_exceptions=False):
+        super(NotEqualTo, self).__init__(self.__compare, falsey_exceptions)
+        self.value = value
+
+    def __compare(self, value, **flags):
         """
         TODO: Document
         """
         return value != self.value
+
+ne = not_equal_to = NotEqualTo
+"""
+An alias to the NotEqualTo class.
+"""
 
 
 class GreaterThanEqualTo(Comparison):
     """
     A greater than or equal to comparison.
     """
-    def __call__(self, value, **flags):
+    def __init__(self, value, falsey_exceptions=False):
+        super(GreaterThanEqualTo, self).__init__(self.__compare, falsey_exceptions)
+        self.value = value
+
+    def __compare(self, value, **flags):
         """
         TODO: Document
         """
         return value >= self.value
+
+gte = greater_than_equal_to = GreaterThanEqualTo
+"""
+An alias to the GreaterThanEqualTo class.
+"""
 
 
 class GreaterThan(Comparison):
     """
     A greater than comparison.
     """
-    def __call__(self, value, **flags):
+    def __init__(self, value, falsey_exceptions=False):
+        super(GreaterThan, self).__init__(self.__compare, falsey_exceptions)
+        self.value = value
+
+    def __compare(self, value, **flags):
         """
         TODO: Document
         """
         return value > self.value
+
+gt = greater_than = GreaterThan
+"""
+An alias to the GreaterThan class.
+"""
 
 
 class __ValueIs(object):
