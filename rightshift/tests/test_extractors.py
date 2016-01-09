0e2c7474fefc0d3a61ed685c484cfc3971b311cb
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2016-01-09 16:06:13 +0200

    #5: Added test for item extractor with dictionaries.

--- rightshift/tests/test_extractors.py
+++ rightshift/tests/test_extractors.py
@@ -2,8 +2,9 @@ from math import isnan
 
 from rightshift.extractors import item, attr, pattern_group
 from hypothesis import given, assume
-from hypothesis.strategies import text, integers, floats, lists, booleans, one_of
-from random import randint
+from hypothesis.strategies import text, integers, floats, lists, booleans
+from hypothesis.strategies import dictionaries, one_of
+from random import randint, choice
 
 
 def __common_item_tests(data):
@@ -22,5 +23,15 @@ def test_item_with_text(data):
 @given(lists(one_of(text(), integers(), floats(), booleans())))
 def test_item_with_lists(data):
     __common_item_tests(data)
+
+
+@given(dictionaries(keys=text(), values=one_of(text(), integers(), floats(),
+                                               booleans())))
+def test_item_with_dictionaries(data):
+    assume(len(data) > 0)
+    key = choice(data.keys())
+    if isinstance(data[key], float):
+        assume(not isnan(data[key]))
+    assert item[key](data) == data[key]
 
 
