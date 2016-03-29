d14b3960cc32fdf9a9003c62b99defeb3ec41df9
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2016-03-29 16:00:31 +0200

    #32: Completed doc comments for rightshift.conditionals module.

--- rightshift/conditionals.py
+++ rightshift/conditionals.py
@@ -28,21 +28,34 @@ brk and Break reference an instance of the rightshift.conditionals.Break class.
 class ConditionException(TransformationException):
     """
     ConditionException is a base class for all exceptions that may be thrown
-    by Condition instances during instantiation or break checking.
+    by BooleanCondition instances during instantiation or break checking.
     """
 
 
 class Condition(Transformer):
     """
-    Condition is the base class for transformers that
     """
     def __init__(self, matcher):
-        """
-        """
         if not isinstance(matcher, Matcher):
             raise ConditionException('matcher parameter must be an instance of '
                                      'rightshift.matchers.Matcher')
         self.matcher = matcher
+
+
+class BooleanCondition(Condition):
+    """
+    BooleanCondition is a base class for transformers that use a Matcher
+    instance to determine which one of two transformers should be executed and
+    its result returned.
+
+    By default the Identity transformer will be executed when the Matcher
+    succeeds or fails but this can be changed by calling the .then() and
+    .otherwise() methods on the BooleanCondition instance.
+    """
+    def __init__(self, matcher):
+        """
+        """
+        super(BooleanCondition, self).__init__(matcher)
         self.then_transformer = identity
         self.otherwise_transformer = identity
 
@@ -65,8 +78,11 @@ class Condition(Transformer):
         return self
 
 
-class WhenCondition(Condition):
+class WhenBooleanCondition(BooleanCondition):
     """
+    The WhenBooleanCondition evaluates the matcher and in the event of a True
+    result the .then() transformer is called, otherwise the .otherwise()
+    transformer is called.
     """
     def __call__(self, value, **flags):
         """
@@ -76,13 +92,17 @@ class WhenCondition(Condition):
         else:
             return self.otherwise_transformer(value, **flags)
 
-when = WhenCondition
+when = WhenBooleanCondition
 """
+when is an alias to the WhenBooleanCondition within this module.
 """
 
 
-class WhenNotCondition(Condition):
+class WhenNotBooleanCondition(BooleanCondition):
     """
+    The WhenNotBooleanCondition evaluates the matcher and in the event of a
+    False result the .then() transformer is called, otherwise the .otherwise()
+    transform is called.
     """
     def __call__(self, value, **flags):
         if not self.matcher(value, **flags):
@@ -90,13 +110,16 @@ class WhenNotCondition(Condition):
         else:
             return self.otherwise_transformer(value, **flags)
 
-when_not = WhenNotCondition
+when_not = WhenNotBooleanCondition
 """
+when_not is an alias to the WhenNotBooleanCondition within this module.
 """
 
 
-class BreakIfCondition(WhenCondition):
+class BreakIfCondition(WhenBooleanCondition):
     """
+    The BreakIfCondition inherits from the WhenBooleanCondition. When the
+    matcher is evaluated and returns True an exception will be raised.
     """
     def __init__(self, matcher):
         super(BreakIfCondition, self).__init__(matcher)
@@ -104,14 +127,19 @@ class BreakIfCondition(WhenCondition):
 
 break_if = BreakIfCondition
 """
+break_if is an alias to the BreakIfCondition within this module.
 """
 
 
-class BreakIfNotCondition(WhenNotCondition, BreakIfCondition):
+class BreakIfNotCondition(WhenNotBooleanCondition, BreakIfCondition):
     """
+    The BreakIfNotCondition inherits from the WhenNotBooleanCondition and the
+    BreakIfCondition. When the matcher is evaluated and returns False an
+    exception will be raised.
     """
 
 
 break_if_not = BreakIfNotCondition
 """
+break_if_not is an alias to BreakIfNotCondition within this module.
 """
